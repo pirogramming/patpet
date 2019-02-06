e3639497d931537daa27782f4151ef6e8872d63c
yujin75 <wkddbwls514@naver.com>
2019-02-06 15:08:03 +0900

    Post 모델 수정1

--- home/models.py
+++ home/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, verbose_name='제목')
     content = models.TextField(verbose_name='내용')
-    photo = models.ImageField(blank=True)
+    photo = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)  # 길이 제한 있는 문자열
     updated_at = models.DateTimeField(auto_now=True)  # 길이 제한 없는 문자열
