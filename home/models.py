45cfc6ba369a7ad8f633711524091f867da4ae45
yujin75 <wkddbwls514@naver.com>
2019-02-06 17:18:56 +0900

    되어라

--- home/models.py
+++ home/models.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.db import models
 
+
 class Post(models.Model):
-    content = models.TextField(verbose_name='내용')
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
     photo = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)  # 길이 제한 있는 문자열
     updated_at = models.DateTimeField(auto_now=True)  # 길이 제한 없는 문자열
