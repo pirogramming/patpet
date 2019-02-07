15407b69a6259d029bcb2513dc2d34db38bae8b4
yongsk0066 <yongsk0066@naver.com>
2019-02-07 12:52:57 +0900

    로그인 폼 연결 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from .forms import SignupForm
@@ -9,12 +10,12 @@ from allauth.socialaccount.templatetags.socialaccount import get_providers
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = SignupForm()
+        form = UserCreationForm()
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from home import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
 ]
