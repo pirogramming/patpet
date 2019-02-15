14c6ec3aa79a5dd7698e5a5c3c0d3851a2d9a0c4
YongSeokJang <yongsk0066@naver.com>
2019-02-15 15:57:29 +0900

    explore 까지 merge

--- explore/urls.py
+++ explore/urls.py
@@ -4,6 +4,10 @@ from explore import views
 
 app_name= 'explore'
 urlpatterns = [
-    path('', views.explore_post_list, name='explore'),
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
     # path('explore/', views.post_list, name='explore'),
+    path('new/', views.post_new, name='post_new'),
+    path('<int:id>/edit/', views.post_edit, name='post_edit'),
+
     ]
