8549ab24b6d7ea5c33d4567c3cb14f37d5a66708
yujin75 <wkddbwls514@naver.com>
2019-02-17 20:09:33 +0900

    아이콘 클릭 시 댓글 입력창 뜨도록 & 자기 댓글만 지울 수 있게

--- my_profile/views.py
+++ my_profile/views.py
@@ -61,27 +61,25 @@ def post_delete(request, pk):
 
 @login_required
 def comment_new(request, pk):
-    # 요청 메서드가 POST방식 일 때만 처리
     if request.method == 'POST':
-        # Post인스턴스를 가져오거나 404 Response를 돌려줌
         post = get_object_or_404(Post, pk=pk)
-        # request.POST에서 'content'키의 값을 가져옴
         content = request.POST.get('content')
-        #
-        # # 'content'키가 없었거나 내용이 입력되지 않았을 경우
         if not content:
-            # 400(BadRequest)로 응답을 전송
             return HttpResponse('댓글 내용을 입력하세요', status=400)
 
-        # 내용이 전달 된 경우, Comment객체를 생성 및 DB에 저장
         Comment.objects.create(
             post=post,
-            # 작성자는 현재 요청의 사용자로 지정
             author=request.user,
             content=content
         )
 
+        return redirect('home:post_list')
 
-        # 정상적으로 Comment가 생성된 후
-        # 'post'네임스페이스를 가진 url의 'post_list'이름에 해당하는 뷰로 이동
+
+@login_required
+def comment_delete(request, pk):
+    if request.method == 'POST':
+        comment = get_object_or_404(Comment, pk=pk)
+        comment.delete()
+        messages.success(request, '삭제완료')
         return redirect('home:post_list')
